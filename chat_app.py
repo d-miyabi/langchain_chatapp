bc81e5df9578e84ec35fdb56d241058bef5a9b61
Komatsubara Masato <m-koma@nifty.com>
2023-12-22 17:06:06 +0900

    正解情報をクッキーで保存

--- chat_app.py
+++ chat_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 import pandas as pd
+import extra_streamlit_components as stx
+from datetime import datetime, timedelta
 from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
@@ -9,10 +11,49 @@ from langchain.schema import (
     AIMessage
 )
 
-def authenticate_user():
-    
-    print("*****************************************************************")
+cookie_manager = stx.CookieManager(key="cookie")
 
+
+from datetime import datetime, timedelta
+
+def get_expire_date():
+    # Getting today's date
+    today = datetime.now()
+
+    # Adding 30 days
+    future_date = today + timedelta(days=30)
+
+    return future_date
+
+
+def set_cookie():
+    cleared_questions = st.session_state.cleared_questions
+    # st.subheader("Set Cookie:")
+    # cookie = st.text_input("Cookie", key="1")
+    val = list_to_string(cleared_questions)
+    # if st.button("Add"):
+    cookie_manager.set("cleared_questions", val, expires_at=get_expire_date())
+
+
+def list_to_string(list):
+    return ', '.join(str(x) for x in list)
+
+
+def string_to_list(s):
+    # 数値が直接渡された場合、その数値を含むリストを返す
+    if isinstance(s, int):
+        return [s]
+
+    # 文字列が渡された場合の処理
+    try:
+        # 文字列をカンマで分割し、空白を削除した後に整数に変換
+        return [int(x.strip()) for x in s.split(',') if x.strip().isdigit()]
+    except ValueError:
+        # 数値以外の文字が含まれている場合、エラーメッセージを返す
+        return "入力された文字列は数値に変換できません。"
+
+
+def authenticate_user():
     credentials = {
         "usernames": {
             "test": {
@@ -26,46 +67,31 @@ def authenticate_user():
         credentials=credentials,
         cookie_name="cookie_name",
         key="some_signature_key",
-        cookie_expiry_days=30
+        cookie_expiry_days=0
     )
 
     # ログインページの表示
     name, authentication_status, username = authenticator.login('Login', 'main')
 
-    print("クリック！")
-
-    if authentication_status:
-        print("ok")
+    if st.session_state['authentication_status']:
         st.sidebar.title("テーマ一覧")
         return True
-    elif authentication_status == False:
-        print("false")
+    elif st.session_state['authentication_status'] == False:
         st.error("Username/password is incorrect")
         return False
-    elif authentication_status == None:
-        print("none")
+    elif st.session_state['authentication_status'] == None:
         st.warning("Please enter your username and password")
         return False
 
 def init_page():
-    print("init")
-
-    # if "not_first_time" in st.session_state:
-    #     return
-
-    # st.set_page_config(
-    #     page_title="Webアプリ用語100本ノック"
-    # )
     st.header("面接対策100本ノック")
     st.write("このアプリは、面接時によく聞かれるWebアプリに関連する専門用語について、回答の仕方を練習するものです。")
     st.write("左のサイドバーから、取り組みたいテーマを選択してください。")
 
-    # st.session_state.not_first_time = True
-
 
 def init_messages():
     role = "あなたはエンジニア採用を行う面接官です。あなたの問いに対して入社希望者が回答したら、その回答に対して内容が妥当か判断してください。正しい場合は、「よく理解されていますね」と答えた上で、必要に応じて補足を行ってください。不足や誤りがある場合は、正解は提示せずに、再度考えるよう促してください"
-    # clear_button = st.sidebar.button("Clear Conversation", key="clear")
+
     if "messages" not in st.session_state:
         st.session_state.messages = [
             SystemMessage(content=role),
@@ -87,13 +113,8 @@ def find_dictionary_by_id(id_to_find):
     return None
 
 
-# def init_correct_num():
-    # st.sidebar.write("正解数: ", st.session_state.correct_answers_num)
-
-
 def select_model():
     model_name = "gpt-4"
-    # temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
     temperature = 0
     return ChatOpenAI(temperature=temperature, model_name=model_name, streaming=True)
 
@@ -118,19 +139,38 @@ def create_dict_from_excel():
 def display_questions():
     for item in st.session_state.questions_list:
         if item['id'] % 100 == 0:
-            # idが100で割り切れる場合、サイドバーにH2を追加
             with st.sidebar:
                 st.header(item['title'] + "に関するテーマ")
         else:
-            st.sidebar.button(item['title'], on_click=set_current_question, args=(item['id'],))
+            if item['id'] in st.session_state.cleared_questions:
+                item_name = "[ ○ ] " + item['title']
+                st.sidebar.button(item_name, on_click=set_current_question, args=(item['id'],))
+
+            else:
+                item_name = "[ - ] " + item['title']
+                st.sidebar.button(item_name, on_click=set_current_question, args=(item['id'],))
 
 
 def set_current_question(id):
     st.session_state.current_question_id = id
 
 
+def register_cookie_to_state():
+    if not "cleared_questions" in st.session_state:
+        value = cookie_manager.get(cookie="cleared_questions")
+        if value:
+            st.session_state.cleared_questions = string_to_list(value)
+        else:
+            st.session_state.cleared_questions = []
+
+
 def main():
+    # 検証用のボタン
+    # if st.button("Delete"):
+    #     cookie_manager.delete("cleared_questions")
+
     init_page()
+    register_cookie_to_state()
 
     create_dict_from_excel()
     display_questions()
@@ -149,8 +189,7 @@ def main():
         elif isinstance(message, HumanMessage):
             with st.chat_message('user'):
                 st.markdown(message.content)
-        # else:  # isinstance(message, SystemMessage):
-            # st.write(f"System message: {message.content}")
+
 
     # ユーザーの入力を監視
     user_input = st.chat_input("こちらに回答を入力してください")
@@ -160,8 +199,9 @@ def main():
         with st.chat_message("assistant"):
             st_callback = StreamlitCallbackHandler(st.container())
             response = llm(messages, callbacks=[st_callback])
-            # if "正解です" in response.content:
-            #     st.session_state.correct_answers_num += 1
+            if "よく理解されていますね" in response.content:
+                st.session_state.cleared_questions.append(st.session_state.current_question_id)
+                set_cookie()
 
         st.session_state.messages.append(AIMessage(content=response.content))
 
@@ -169,11 +209,6 @@ def main():
             st.session_state.messages.append(AIMessage(content=response.content))
 
 
-
-
-    # init_correct_num()
-
 if __name__ == '__main__':
     if authenticate_user():
-        # init_page()
         main()
--- hash_test.py
+++
@@ -1,5 +0,0 @@
-import streamlit_authenticator as stauth
-
-hashed_password = stauth.Hasher(['password']).generate()
-print(hashed_password)
-
